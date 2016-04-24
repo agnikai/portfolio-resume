1fe21b8f85681a2674bda77d9400c3693eecca12
Jacquelyn Carver <jacquelyn905@gmail.com>
2016-04-24 15:52:41 -0400

    Initialized lightgalleries and edited require statements.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,14 +1,16 @@
-//= require jquery.js
+//= require jquery/dist/jquery.js
 //= require jquery.aniview.js
-//= require jquery.aniview.min.js
-
+//= require lightgallery/dist/js/lightgallery-all.js
 
 
  $(document).ready(function(){
+
+ 	$("#lightgallery").lightGallery();
+ 	$("#edit-lightgallery").lightGallery();
 
 	var options = {
 	    animateThreshold: 200,
 	    scrollPollInterval: 20
 	}
-	$('.aniview').AniView(options);
+	$('.aniview').AniView(options); 
 });
